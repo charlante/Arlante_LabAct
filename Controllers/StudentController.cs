51c534853a227fcd99a2bc8576541437e2be8f53
charlante <144820692+charlante@users.noreply.github.com>
2023-09-20 23:08:36 +0800

    Initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Arlante_LabAct.Models;
+
+namespace sample.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> StudentList = new List<Student>
+            {
+                new Student()
+                {
+                    Id= 1,FirstName = "Gabriel",LastName = "Montano", Course = Course.BSIT, AdmissionDate = DateTime.Parse("2022-08-26"), GPA = 1.5, Email = "ghaby021@gmail.com"
+                },
+                new Student()
+                {
+                    Id= 2,FirstName = "Zyx",LastName = "Montano", Course = Course.BSIS, AdmissionDate = DateTime.Parse("2022-08-07"), GPA = 1, Email = "zyx@gmail.com"
+                },
+                new Student()
+                {
+                    Id= 3,FirstName = "Aerdriel",LastName = "Montano", Course = Course.BSCS, AdmissionDate = DateTime.Parse("2020-01-25"), GPA = 1.5, Email = "aerdriel@gmail.com"
+                }
+            };
+        public IActionResult Index()
+        {
+
+            return View(StudentList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            Student? student = StudentList.FirstOrDefault(st => st.Id == id);
+
+            if (student != null)
+                return View(student);
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent(Student newStudent)
+        {
+            StudentList.Add(newStudent);
+            return View("Index", StudentList);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateStudent(int id)
+        {
+            Student? student = StudentList.FirstOrDefault(st => st.Id == id);
+
+            if (student != null)
+                return View(student);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateStudent(Student studentChanges)
+        {
+            Student? student = StudentList.FirstOrDefault(st => st.Id == studentChanges.Id);
+
+            if (student != null)
+            {
+                student.FirstName = studentChanges.FirstName;
+                student.LastName = studentChanges.LastName;
+                student.Email = studentChanges.Email;
+                student.Course = studentChanges.Course;
+                student.GPA = studentChanges.GPA;
+                student.AdmissionDate = studentChanges.AdmissionDate;
+            }
+            return View("Index", StudentList);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Arlante_LabAct.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
